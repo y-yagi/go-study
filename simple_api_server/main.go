c67ddeea574654aab6b3070e30dd25bead2231cd
yuuji.yaginuma <yuuji.yaginuma@gmail.com>
2016-03-18 07:28:16 +0900

    add debug log

--- simple_api_server/main.go
+++ simple_api_server/main.go
@@ -48,7 +48,7 @@ func getPosts(db *sql.DB) ([]Post, error) {
 }
 
 func (ph *postHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	fmt.Printf("debug log\n")
+	fmt.Printf("Started %s %s for %s at %s\n", r.Method, r.RequestURI, r.RemoteAddr, time.Now().Format(time.RFC3339))
 	fmt.Fprint(w, ph.format)
 }
 
