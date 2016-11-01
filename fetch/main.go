5f5d49704e155d437041fb9104b45b1fb155e5ee
yuuji.yaginuma <yuuji.yaginuma@gmail.com>
2016-11-02 07:40:16 +0900

    add fetch

---
+++ fetch/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"os"
+)
+
+func main() {
+	if len(os.Args) == 1 {
+		fmt.Fprintln(os.Stderr, "Please specify url")
+		os.Exit(1)
+	}
+
+	for _, url := range os.Args[1:] {
+		resp, err := http.Get(url)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "fetch: %v\n", err)
+			os.Exit(1)
+		}
+
+		defer resp.Body.Close()
+
+		body, err := ioutil.ReadAll(resp.Body)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "fetch: reading %s: %v\n", url, err)
+			os.Exit(1)
+		}
+		fmt.Printf("%s", body)
+	}
+}
